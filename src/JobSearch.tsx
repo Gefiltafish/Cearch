32403b79d0e05d11ee304b3329af8bd933d198e6
efedericomedina <e.federico.medina@gmail.com>
2024-06-27 14:39:46 +0200

    ai process

--- src/JobSearch.tsx
+++ src/JobSearch.tsx
@@ -1,18 +1,49 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Groq from "groq-sdk";
+
+const groq = new Groq({
+  apiKey: import.meta.env.VITE_GROQ_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+export async function getGroqChatCompletion(content: string) {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `create a JSON list called keywords with the keywords of only the required technologies by the job posting: ${content}`,
+      },
+    ],
+    model: "llama3-8b-8192",
+    response_format: { type: "json_object" },
+  });
+}
+
 export const JobSearch = () => {
-  const [showResults, setShowResults] = useState<boolean>();
+  const [jobDescription, setJobDescription] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [techs, setTechs] = useState<[]>([]);
   const navigate = useNavigate();
 
-  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+  const handleChange = (event: any) => {
+    setJobDescription(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      setShowResults(true);
-    }, 1500);
+
+    const chatCompletion = await getGroqChatCompletion(jobDescription);
+    const techs = JSON.parse(
+      chatCompletion.choices[0]?.message?.content || "{}"
+    );
+
+    console.log(techs);
+
+    setIsLoading(false);
+    setTechs(techs.keywords || []);
   };
   const handleGoPress = () => {
     setIsLoading(true);
@@ -30,51 +61,24 @@ export const JobSearch = () => {
             <textarea
               placeholder="Paste job description"
               style={{ height: "24rem", width: "36rem" }}
+              value={jobDescription}
+              onChange={handleChange}
             ></textarea>
             <button type="submit">Generate</button>
           </div>
         </form>
       </div>
-
-      {showResults && (
+      {isLoading && <span>Loading...</span>}
+      {techs.length > 0 && (
         <div style={{ height: "24rem", width: "36rem" }}>
           <ul style={{ listStyle: "none" }}>
-            <li>
-              <label>Typescript</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Java</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>React</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Preact</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Management</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Vue</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Angular</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Friendly</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
-            <li>
-              <label>Office bound</label>
-              <input type="checkbox" defaultChecked></input>
-            </li>
+            {techs.map((tech) => (
+              <li key={tech}>
+                <label>{tech}</label>
+                <input type="checkbox" defaultChecked></input>
+              </li>
+            ))}
+            {/* Example techs to be displayed from the job description  */}
           </ul>
           <button type="button" onClick={handleGoPress}>
             Find consultants
